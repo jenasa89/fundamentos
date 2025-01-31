09cda41a9093446eb9f039f4cffc8be79fed87f1
Dieote <gomezmdiego97@gmail.com>
2025-01-31 14:24:22 +0100

    Creacion clase carta

--- Blackjack2/Blackjack2/obj/Debug/net9.0/Blackjack2.AssemblyInfo.cs
+++ Blackjack2/Blackjack2/obj/Debug/net9.0/Blackjack2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a060d0f10a87391b1481d2dfb0d7d2cb69b95dbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cdb8eaea67b8846495aba157b79300a94cf577a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
